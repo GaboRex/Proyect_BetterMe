be081f6cff61871ae75ddc458782aa55421388ae
Gabriel Neme Benitez <gaboneme11@gmail.com>
2022-08-16 21:51:17 -0400

    prueba base de datos

---
+++ Backend/src/config/db.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const mysql = require("mysql");
+//TODO: change to Environment variables
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+connection.connect(function (err) {
+  if (err) {
+    return console.error("error: " + err.message);
+  }
+  console.log("Connected to the MySQL server.");
+});
+
+// Create a pool connection
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+module.exports = connection;
