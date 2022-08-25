efb00b0594a45384337dae3d21ac64ecb4b731f0
Pabdro <pablis.badani@gmail.com>
2022-08-25 17:46:48 -0400

    menu ziiiiiiiiiiiiiiiiiii

--- Frontend/betterme/src/index.js
+++ Frontend/betterme/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import {BrowserRouter} from 'react-router-dom'
+import Menu from './Paginas/Menu/menu';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
