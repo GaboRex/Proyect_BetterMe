8b9e0440be64f0b5f9247bcded2fbc1884489d7c
Pabdro <pablis.badani@gmail.com>
2022-08-25 22:38:40 -0400

    sql2

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@
 // })
 
 const express = require('express')
-const mysql = require('mysql')
+const mysql = require('mysql2')
 const cors = require('cors')
 const app = express()
 
---
+++ Frontend/betterme/src/Paginas/Menu/menu.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+// import './menu.css';
+import dietaImage from './dieta.jpeg';
+import pesasSinDuenio from './pesasSinDuenio.jpeg';
+import seniorJalando from './seniorJalando.jpeg';
+
+const Menu = () => {
+    return(
+        <div className="Menu">
+            <br/><br/><br/><br/><br/><br/>
+            <div className="actividades">
+        <h10><b>Actividades</b></h10>
+        </div>
+        <div className="parce">
+        <img src={pesasSinDuenio}/>
+        <div>
+            <h11><b>Ejercicios recomendados</b></h11>
+            </div>
+            <button type="button">IR</button>
+        </div>
+        <div className="parce">
+            <img src={seniorJalando}/>
+            <div>
+            <h11><b>Crear tu rutina</b></h11>
+            </div>
+            <button type="button">IR</button>
+        </div>
+        <div className="parce">
+            <img src={dietaImage}/>
+            <div>
+            <h11><b>Dieta saludable</b></h11>
+            </div>
+            <button type="button">IR</button>
+        </div>
+        </div>
+    )
+}
+export default Menu;
