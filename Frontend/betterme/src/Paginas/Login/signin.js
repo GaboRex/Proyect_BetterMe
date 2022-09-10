97213ad82e40289ec0c9d87ff20020c6c11b5a41
Sebastian Rojas <sebosinaga@gmail.com>
2022-09-10 17:32:33 -0400

    Fix login form

--- Frontend/betterme/src/Paginas/Login/signin.js
+++ Frontend/betterme/src/Paginas/Login/signin.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import './login.css';
 import API from '../../config/api';
 
@@ -8,27 +8,39 @@ function Login() {
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
   //const [user, setUser] = useState({})
 
   const [loginStatus, setLoginStatus] = useState('Hello')
 
-  const login = () => {
-    API.post('/getUsuario', {
+  const login = async () => {
+    
+    try{
+    const response = await API.post('/getUsuario', {
       username: username,
       password: password
-    }).then((response) => {
-      if(response.data.message) {
-        setLoginStatus(response.data.message)
-      } else {
-        setLoginStatus(response.data[0].nombre)
-      }
     })
-    
+    debugger
+    const { data } = response
+    const { usuario } = data
+    debugger
+    if(usuario.length > 0) {
+    setLoginStatus(usuario[0]?.nombre)
+    debugger
+    navigate('/menu')
+    } else {
+      setLoginStatus('Usuario o contraseña incorrectos')
+    }
+  } catch (err) {
+    console.log(err)
   }
+  }
+
 
   return (
     <div className="Login">
-      <form className="form1">
+      {/* <form className="form1"> */}
         <h1 className="iniSe">INICIAR SESION</h1>
         <div className="form_container">
           <div className="form_group">
@@ -44,19 +56,18 @@ function Login() {
         </div>
         {/* <br /><br /><br /> */}
         <div className="footer1">
-          <Link to="/">
+          
+          {/* <Link to="/">
             <button className="btn_cancelar">Cancelar</button>
-          </Link>
+          </Link> */}
           
           <button onClick={login} className="btn_iniSe">Iniciar Sesión</button>
-          <Link to="/menu">
-            
-          </Link>
-          <Link to='/signup'>
+          
+          {/* <Link to='/signup'>
             <button className="btn_crear">Crear una cuenta</button>
-          </Link>
+          </Link> */}
         </div>
-      </form>
+      {/* </form> */}
       <h1>{loginStatus}</h1>
     </div>
   )
